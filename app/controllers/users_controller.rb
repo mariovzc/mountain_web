1d0b4e527b70fa98a18a55ef03b37bf63af5f0aa
Mario Vizcaino <mario.vizcainos@gmail.com>
2018-04-21 21:31:47 -0500

    refactor of users display

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_request, only: [:login]
+  before_action :set_user, only: [:update]
+  before_action :get_users, only: [:create, :update]
+
   def index
     @users = User.all
   end
   def create
-    user = User.new(user_params)
-  
-    if user.save
-      @users = User.all
-    else
+    user = User.new(user_params)  
+    unless user.save
       render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    unless @user.update(user_params)
+      render json: { errors: @user.errors.full_messages }, status: :bad_request
     end
   end
   
@@ -27,5 +33,12 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :document, :phone)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def get_users
+    @users = User.all
   end
 end
